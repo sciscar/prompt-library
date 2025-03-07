0c279add03ea4874e14d4564d908cd5c79216024
sciscar <sciscar@ieseljust.com>
2025-03-07 12:30:11 +0100

    Updates

---
+++ precache-manifest.108e95cc9b06e25836d19a927883efcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00193d3eb98fd178a266375d69fc000",
+    "url": "/prompt-library/index.html"
+  },
+  {
+    "revision": "f13884a0347c0ce65568",
+    "url": "/prompt-library/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/prompt-library/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/prompt-library/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "f13884a0347c0ce65568",
+    "url": "/prompt-library/static/js/main.c5c0def7.chunk.js"
+  },
+  {
+    "revision": "aa083d5b3077ef042c54",
+    "url": "/prompt-library/static/js/runtime~main.7380ea36.js"
+  }
+]);
